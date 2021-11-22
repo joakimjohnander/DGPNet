43c11909a9609fa2069febb6eb8563985c15dccf
Joakim <joakim.johnander@zenseact.com>
2021-11-22 08:30:10 +0100

    Adds local config and removes it from gitignore.

---
+++ local_config.py
@@ -0,0 +1,10 @@
+
+
+config = {
+    'workspace_path': "/workspace/DGPNet",
+    'plot_path': "/workspace/DGPNet/plots",
+    'visualization_path': "/workspace/DGPNet/visualization",
+    'checkpoint_path': "/workspace/DGPNet/checkpoints",
+    'pascal_path': "/my_data/pascal",
+    'coco_path': "/my_data/coco",
+}
